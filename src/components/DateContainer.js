a97e890c6c6e7817c8a5db368aeaf0a652c8b324
khoibv92 <144824127+khoibv92@users.noreply.github.com>
2024-04-05 09:28:20 +0700

    Add files via upload

---
+++ src/components/DateContainer.js
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import DateDisplay from "./DateDisplay"
+import "./date.css"
+
+const DateContainer = () => {
+    const [date, setDate] = useState(Date.now())
+    return ( 
+        
+        <>
+            <input type="date" className="date-input" onChange={(e) => setDate(e.target.value)}/>
+            <DateDisplay date={date}/>
+        </>
+     );
+}
+ 
+export default DateContainer;
